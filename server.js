e8da6e9ad585511943eb79a8ab690753f150d70b
Mostafa Hashhash <mostafasaeed483@gmail.com>
2019-11-22 23:41:52 +0200

    add data and route

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.listen(8000, function() {
-	console.log("Running Server")
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
 })
+
+app.listen(8000,function(){console.log('server is listening')})
+
